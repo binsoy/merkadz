f5ccbc4a8983c7d2df10157f6a07eb8dbb12455a
Vince Elthon Pacarat <veopacarat@gmail.com>
2020-08-23 15:55:22 +0800

    heroku deploy

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
 
+// for Heroku
+const PORT = process.env.PORT || 8080;
+
 const userRouter = require("./handlers/user");
 const postRouter = require("./handlers/post");
 
@@ -10,7 +13,7 @@ dotenv.config();
 
 //connect to DB
 mongoose.connect(
-  process.env.DB_CONNECT,
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -25,4 +28,9 @@ app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
 
-app.listen(3000, () => console.log("Sever up and running"));
+// Step 3: for React deployment
+// if(process.env.NODE_ENV === 'production'){
+//   app.use(express.static('client/build'))
+// }
+
+app.listen(PORT, () => console.log("Sever up and running"));
